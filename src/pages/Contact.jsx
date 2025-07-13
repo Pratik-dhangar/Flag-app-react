4d8938c74e8876180a4f9e066d34e55669b0274c
Pratik-dhangar <dhangarpratik5077@gmail.com>
2025-07-13 12:39:42 +0530

    contactPage

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,48 @@
 export const Contact = () => {
+  const handleFormSubmit = (formData) => {
+    // console.log(formData.entries());
+    const formInputData = Object.fromEntries(formData.entries());
+    console.log(formInputData);
+  };
+
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <section className="section-contact">
+      <h2 className="container-title">Contact Us</h2>
+
+      <div className="contact-wrapper container">
+        <form action={handleFormSubmit}>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="enter your name"
+            name="username"
+            required
+            autoComplete="off"
+          />
+
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter you email"
+            name="email"
+            required
+            autoComplete="off"
+          />
+
+          <textarea
+            className="form-control"
+            rows="10"
+            placeholder="Enter your message"
+            name="message"
+            required
+            autoComplete="off"
+          ></textarea>
+
+          <button type="submit" value="send" class="btn btn-outline-primary btn-lg" style={{padding:"1.5rem", border:"0.3px solid black", fontSize:"1.6rem", color:"black"}}>
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
   );
 };
