f2aeacd82c5bc1b7d02148b6425d66c3c8562d75
Pratik-dhangar <dhangarpratik5077@gmail.com>
2025-07-13 17:26:42 +0530

    error

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -12,7 +12,7 @@ export const ErrorPage = () => {
        background:"#cfbdbc"
       }}
     >
-      <h1 className="p-5" style={{margin:"auto"}}>Oops! An error occurred.</h1>
+      <h1 className="p-5" style={{margin:"auto", fontSize:"50px"}}>Oops! An error occurred.</h1>
       {error && <p style={{margin:"auto", background:"red", padding:"2rem"}}>{error.data}</p>}
       <NavLink to="/" className="mb-3">
         <button className="p-3" style={{ width: "auto", margin:"3rem auto" }}>Go Home</button>
